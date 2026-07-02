d563125f7c1597f39119020b76e206750ed63998
Lauren Knill <laurknill@gmail.com>
2026-07-01 19:30:27 -0500

    Init: Setup, began basic boomerang physics

---
+++ src/Server/Referential/ThrownWeapons.lua
@@ -0,0 +1,24 @@
+local ThrownWeapons = {}
+
+ThrownWeapons.StateEnum = {
+   Outgoing = 1,
+   Exhausted = 2,
+   Returning = 3
+} :: {
+   ["Outgoing"]: number,
+   ["Exhausted"]: number,
+   ["Returning"]: number
+}
+
+export type Data = {
+   ToolId: string,
+   State: number,
+   ReplicationPart: Part
+}
+
+-- // Data for a thrown weapon out on the field
+ThrownWeapons.Data = {} :: {
+   [Player]: Data
+}
+
+return ThrownWeapons
