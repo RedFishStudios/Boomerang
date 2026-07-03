1e8a6d221a4f250233340030d888e89970ac9cbc
Lauren Knill <laurknill@gmail.com>
2026-07-03 12:35:59 -0500

    Set up basic round cycling; generates next map and gamemode options; placeholder random selection for testing

--- src/Server/Referential/CurrentGameState.lua
+++ src/Server/Referential/CurrentGameState.lua
@@ -1,10 +1,16 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TasksList = require(ReplicatedStorage.Shared.Classes.TasksList)
+
+type RoundFinishedTask = ()->()
+
 local CurrentGameState: {
    IsInRound: boolean,
    RoundTypeId: string?, -- If in a round
    CurrentMapName: string?, -- If in a round
    RemainingTime: number?, -- If in a round
+   RoundFinishedTasks: TasksList.TasksList<RoundFinishedTask>, -- Should be called only from within the GamemodeLogics module
 
-   -- Copied over from RoundTypes data when a round is set
+   -- Copied over from Gamemodes data when a round is set
    CanRespawn: boolean?
 } = {
    IsInRound = false,
@@ -12,5 +18,7 @@ local CurrentGameState: {
    CurrentMapName = nil,
    RemainingTime = nil,
    CanRespawn = nil,
+   RoundFinishedTasks = TasksList.new()
 }
+
 return CurrentGameState
